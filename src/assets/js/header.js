13c414a981f19de00f3a7ef843bd5de1ab21c5e9
slixz0123 <slim.ph2002@gmail.com>
2023-02-14 12:02:30 -0500

    footer

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -10,3 +10,20 @@ menuOpen.addEventListener("click", () => {
 menuClose.addEventListener("click", () => {
   overlay.classList.remove("overlay--active");
 });
+
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
