98b68f3c9839bc4880926afc6bf967b4f8a8c22e
davicho896 <jose.naula.est@tecazuay.edu.ec>
2023-02-14 23:17:21 -0500

    Creacion de Catalogos-Cliente
    Creacion de Catalogos-Cliente
    HTML
    CSS
    JS
    
    si lees esto, que mas ve :dog:

--- src/app/modules/cliente/catalogo-renta-cliente/pages/catalogo-renta-cliente/catalogo-renta-cliente.component.ts
+++ src/app/modules/cliente/catalogo-renta-cliente/pages/catalogo-renta-cliente/catalogo-renta-cliente.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CargarscriptsService } from 'src/assets/cargarscripts.service';
 
 @Component({
   selector: 'app-catalogo-renta-cliente',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class CatalogoRentaClienteComponent {
 
+  constructor(
+    private catalogo: CargarscriptsService,
+  ){
+    catalogo.carga(["Catalogo"]);
+  }
 }
