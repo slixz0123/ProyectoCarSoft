a981ce5b1990cb9e4e553f4b0b3e6339b55f1907
davicho896 <jose.naula.est@tecazuay.edu.ec>
2023-02-21 18:10:54 -0500

    Funcionalidad de todos los paneles
    Union de todas las ventanas

--- src/app/modules/administrador-car/list-empleado/list-empleado-routing.module.ts
+++ src/app/modules/administrador-car/list-empleado/list-empleado-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListEmpleadoComponent } from './pages/list-empleado/list-empleado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:ListEmpleadoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/shared/components/headerlogsidebar-administrador/headerlogsidebar-administrador.component.ts
+++ src/app/shared/components/headerlogsidebar-administrador/headerlogsidebar-administrador.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-headerlogsidebar-administrador',
@@ -7,4 +8,76 @@ import { Component } from '@angular/core';
 })
 export class HeaderlogsidebarAdministradorComponent {
 
+  constructor(
+
+    private router : Router
+  )
+  {
+    {
+ 
+      // Datos xd
+    }
+  }
+  ngOnInit(): void {
+  }
+
+  
+// metodo de redireccion de ruta
+  goToLogin($event: any) :void{
+
+  this.router.navigate(['/Auth'])
+  console.log($event)
+
+ }
+ //Automovil
+
+ goToRegisterCar($event: any) :void{
+
+  this.router.navigate(['/register-car'])
+  console.log($event)
+
+ }
+ goToEdicionCar($event: any) :void{
+
+  this.router.navigate(['/edit-car'])
+  console.log($event)
+
+ }
+ goToListCar($event: any) :void{
+
+  this.router.navigate(['/list-car'])
+  console.log($event)
+
+ }
+
+ //Empleado
+ 
+
+ goToRegisterEmple($event: any) :void{
+
+  this.router.navigate(['/register-employee'])
+  console.log($event)
+
+ }
+ goToEdicionEmple($event: any) :void{
+
+  this.router.navigate(['/edit-employee'])
+  console.log($event)
+
+ }
+ goToListEmple($event: any) :void{
+
+  this.router.navigate(['/list-employees'])
+  console.log($event)
+
+ }
+
+
+
+
+
+
+
+
 }
+ 
