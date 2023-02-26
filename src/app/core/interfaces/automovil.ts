7e1426c7d3b238372b6cd13c7cdb570e39566152
Saul Velecela <claudio.velecela.est@tecazuay.edu.ec>
2023-02-26 17:22:16 -0500

    reg car

--- src/app/core/interfaces/automovil.ts
+++ src/app/core/interfaces/automovil.ts
@@ -5,19 +5,20 @@ import { Usuario } from "../models/usuario";
 export class Automovil{
 
    num_placa?:string;
-   anio?:Date;
+   anio?:string;
    color?:string;
    estado?:string;
    marca?:string;
    modelo?:string;
    tipo_vehiculo?:string;
    foto?: string;
+   id_clase?:number;
 
 
    //relacion
    claseautomovil?: Claseautomovil[] | undefined; //id_clase
    seguro?:Seguro []; //cod_seguro
-   usuario?:Usuario [];// id id de suuario
+   usuario?:Usuario [];// id id de Usuario
 
 
 }
--- src/app/modules/administrador-car/register-car/pages/registercar/registercar.component.ts
+++ src/app/modules/administrador-car/register-car/pages/registercar/registercar.component.ts
@@ -17,13 +17,17 @@ import { UsuarioService } from 'src/app/shared/services/usuario.service';
 @Component({
   selector: 'app-registercar',
   templateUrl: './registercar.component.html',
-  styleUrls: ['./registercar.component.css']
+  styleUrls: ['./registercar.component.css'],
+  template:`
+  <select>
+    <option *ngFor="let clase of claseauto">{{clase}}</option>
+  </select>
+`,
 })
 export class RegistercarComponent {
 
-
   automovil: Automovil = new Automovil(); //Inicialice el objeto automovil.
-
+  modelos: Claseautomovil[]= [];
   constructor(private automovilService: AutomovilService,
     private fotoService: FotoService, private toastr: ToastrService, private ClasesCarro:ClasesCarroService) {}
 
@@ -34,16 +38,17 @@ export class RegistercarComponent {
 
   ngOnInit(): void {
     this.automovil.num_placa = '';
-
     this.automovil.color = '';
     this.automovil.estado = '';
     this.automovil.marca = '';
     this.automovil.modelo = '';
     this.automovil.tipo_vehiculo = '';
     this.automovil.foto = '';
+    this.automovil.id_clase;
 
     localStorage.removeItem('num_placa');
     this.mostrarNotificacion();
+    this.getClasesAuto();
   }
 
   registrarCarro() {
@@ -118,7 +123,10 @@ export class RegistercarComponent {
   }
 
 
-
+getClasesAuto(){
+  this.ClasesCarro.getAll().subscribe(data => {
+    this.modelos = data;
+  });}
 
 
 }
