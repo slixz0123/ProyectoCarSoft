e4493adb49883119025f7ca85fe906d0d81b30c5
slixz0123 <slim.ph2002@gmail.com>
2023-02-26 23:03:05 -0500

    REGISTOR DE CLASE

--- src/app/modules/administrador-car/register-clase/pages/register-clase/register-clase.component.ts
+++ src/app/modules/administrador-car/register-clase/pages/register-clase/register-clase.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { Claseautomovil } from 'src/app/core/models/claseautomovil';
 import { Seguro } from 'src/app/core/models/seguro';
+
 import { ClasesCarroService } from 'src/app/shared/services/clases-carro.service';
+import { SeguroService } from 'src/app/shared/services/seguro.service';
 
 @Component({
   selector: 'app-register-clase',
@@ -10,20 +12,24 @@ import { ClasesCarroService } from 'src/app/shared/services/clases-carro.service
   styleUrls: ['./register-clase.component.css']
 })
 export class RegisterClaseComponent {
-
-  claseautomovil: Claseautomovil = new Claseautomovil();
   seguro: Seguro = new Seguro();
-  constructor( private toastr: ToastrService, private ClasesCarro:ClasesCarroService) {}
+  claseautomovil: Claseautomovil = new Claseautomovil();
+
+  constructor( private toastr: ToastrService, private ClasesCarro:ClasesCarroService , private SeguroServi:SeguroService) {}
 
 
   ngOnInit(): void {
     this.claseautomovil.id_clase = 0;
     this.claseautomovil.nombre = '';
     this.claseautomovil.precio_alquler_dia = 0;
+    this.seguro.cod_seguro= 0;
+    this.seguro.nom_seguro= '';
+    this.seguro.precio_seguro= '';
 
 
 
     localStorage.removeItem('id_clase');
+    localStorage.removeItem('cod_seguro');
 
 
   }
@@ -37,30 +43,30 @@ export class RegisterClaseComponent {
           this.claseautomovil = result;
           localStorage.setItem('id_clase', String(this.claseautomovil.id_clase));
 
-          this.toastr.success('Automovil registrado correctamente');
+          this.toastr.success('seguro registrado correctamente');
         },
         error => {
           console.log(error);
-          this.toastr.error('Error al registrar automóvil');
+          this.toastr.error('Error al registrar seguro');
         }
       )
     }
   }
   registrarSeguro() {
-    if (this.claseautomovil.nombre === '') {
+    if (this.seguro.nom_seguro === '') {
       this.toastr.warning("Verifique que esten correctos los campos")
     } else {
-      this.ClasesCarro.post(this.claseautomovil).subscribe(
+      this.SeguroServi.post(this.seguro).subscribe(
         result => {
           console.log(result);
-          this.claseautomovil = result;
-          localStorage.setItem('id_clase', String(this.claseautomovil.id_clase));
+          this.seguro = result;
+          localStorage.setItem('cod_seguro', String(this.seguro.cod_seguro));
 
-          this.toastr.success('Automovil registrado correctamente');
+          this.toastr.success('seguro registrado correctamente');
         },
         error => {
           console.log(error);
-          this.toastr.error('Error al registrar automóvil');
+          this.toastr.error('Error al registrar seguro');
         }
       )
     }
