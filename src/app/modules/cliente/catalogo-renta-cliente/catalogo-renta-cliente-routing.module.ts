bedd3db06fe1a48904d089c0b8e913dbb224e89f
slixz0123 <slim.ph2002@gmail.com>
2023-03-06 17:38:20 -0500

    catalogo yen cliente

--- src/app/modules/cliente/catalogo-renta-cliente/catalogo-renta-cliente-routing.module.ts
+++ src/app/modules/cliente/catalogo-renta-cliente/catalogo-renta-cliente-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CatalogoRentaComponent } from '../../empleado/catalogo-renta/pages/catalogo-renta/catalogo-renta.component';
+import { CatalogoRentaClienteComponent } from './pages/catalogo-renta-cliente/catalogo-renta-cliente.component';
 
 const routes: Routes = [
   {
     path:'',
-    component: CatalogoRentaComponent
+    component: CatalogoRentaClienteComponent
   }
 ];
 
