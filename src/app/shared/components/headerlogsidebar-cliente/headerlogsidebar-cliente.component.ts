8ce69b52b5fd3a364b066ea2261e9bf0296418d4
slixz0123 <slim.ph2002@gmail.com>
2023-03-06 17:30:28 -0500

    redireccion por rol

--- src/app/shared/components/headerlogsidebar-cliente/headerlogsidebar-cliente.component.ts
+++ src/app/shared/components/headerlogsidebar-cliente/headerlogsidebar-cliente.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-headerlogsidebar-cliente',
@@ -7,20 +8,39 @@ import { Router } from '@angular/router';
   styleUrls: ['./headerlogsidebar-cliente.component.css']
 })
 export class HeaderlogsidebarClienteComponent {
+
+  id: any;
+  nombreUsuario: any;
+  nombreRol: any;
+  nombreFoto: any;
+  nombreLogo: any;
+
+  isSuperAdmin: boolean = false;
+  isClientAdmin: boolean = false;
+  isClient: boolean = false;
+  isPublic: boolean = false;
+
+  displayMaximizable: any;
+  isLogin: boolean = false;
+
+
+
   constructor(
 
-    private router : Router
+    private router : Router,
+    private usuarioService: UsuarioService,
   )
   {
     {
- 
+
       // Datos xd
     }
   }
   ngOnInit(): void {
+     this.obtenerUsuario()
   }
 
-  
+
 // metodo de redireccion de ruta
   goToLogin($event: any) :void{
 
@@ -48,5 +68,58 @@ export class HeaderlogsidebarClienteComponent {
   console.log($event)
 
  }
+
+ obtenerUsuario() {
+  this.id = localStorage.getItem('id');
+  if (this.id != '' && this.id != undefined) {
+    this.usuarioService.getPorId(this.id).subscribe((data) => {
+      console.log(data);
+      if (data != null) {
+        this.isLogin = true;
+
+        this.nombreUsuario = data.persona?.nombre + ' ' + data.persona?.apellido;
+        this.nombreRol = data.rol?.nombre_Rol
+        console.log("Found the user => " + this.nombreFoto);
+
+        switch (data.rol?.nombre_Rol) {
+
+          case 'CLIENTE':
+
+            this.isClient = true;
+
+            break;
+
+          case 'EMPLEADO':
+            this.isSuperAdmin = true;
+            this.isClientAdmin = false;
+            this.isClient = false;
+            this.isPublic = false;
+            break;
+          default:
+            alert('Rol desconocido');
+            break;
+        };
+
+      } else {
+        this.isLogin = false;
+        this.nombreUsuario = 'NULL';
+      }
+    });
+  }
+}
+  verificarCargosClientAdmin(id: any) {
+    throw new Error('Method not implemented.');
+  }
+  cerrarSesion() {
+
+    localStorage.removeItem('id');
+    location.replace('/login-usr');
+  }
+
+
+
+
+
+
 
 }
--- src/app/shared/components/headerlogsidebar/headerlogsidebar.component.ts
+++ src/app/shared/components/headerlogsidebar/headerlogsidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CargarscriptsService } from 'src/assets/cargarscripts.service';
 import { Router } from '@angular/router';
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-headerlogsidebar',
@@ -8,17 +9,34 @@ import { Router } from '@angular/router';
   styleUrls: ['./headerlogsidebar.component.css']
 })
 export class HeaderlogsidebarComponent {
+  id: any;
+  nombreUsuario: any;
+  nombreRol: any;
+  nombreFoto: any;
+  nombreLogo: any;
+
+  isSuperAdmin: boolean = false;
+  isClientAdmin: boolean = false;
+  isClient: boolean = false;
+  isPublic: boolean = false;
+
+  displayMaximizable: any;
+  isLogin: boolean = false;
+
+
+
   constructor(
     private _CargarScripts2: CargarscriptsService,
     private router : Router,
+    private usuarioService: UsuarioService,
 
 
 
   )
 
-  
 
-  
+
+
 
   {
     {
@@ -52,13 +70,14 @@ export class HeaderlogsidebarComponent {
     }
 
 
-    
+
   }
 
-  
+
   ngOnInit(): void {
+    this.obtenerUsuario()
   }
-   
+
 // metodo de redireccion de ruta
 goToLogin($event: any) :void{
 
@@ -111,6 +130,59 @@ goToLogin($event: any) :void{
   console.log($event)
 
  }
+
+ obtenerUsuario() {
+  this.id = localStorage.getItem('id');
+  if (this.id != '' && this.id != undefined) {
+    this.usuarioService.getPorId(this.id).subscribe((data) => {
+      console.log(data);
+      if (data != null) {
+        this.isLogin = true;
+
+        this.nombreUsuario = data.persona?.nombre + ' ' + data.persona?.apellido;
+        this.nombreRol = data.rol?.nombre_Rol
+        console.log("Found the user => " + this.nombreFoto);
+
+        switch (data.rol?.nombre_Rol) {
+
+          case 'CLIENTE':
+
+            this.isClient = true;
+
+            break;
+
+          case 'EMPLEADO':
+            this.isSuperAdmin = true;
+            this.isClientAdmin = false;
+            this.isClient = false;
+            this.isPublic = false;
+            break;
+          default:
+            alert('Rol desconocido');
+            break;
+        };
+
+      } else {
+        this.isLogin = false;
+        this.nombreUsuario = 'NULL';
+      }
+    });
+  }
+}
+  verificarCargosClientAdmin(id: any) {
+    throw new Error('Method not implemented.');
+  }
+  cerrarSesion() {
+
+    localStorage.removeItem('id');
+    location.replace('/login-usr');
+  }
+
+
+
+
+
+
 
 
 }
