df1fd4a1d20ad6ac779ab659e50ab023bdbcb894
slixz0123 <slim.ph2002@gmail.com>
2023-03-07 18:25:12 -0500

    editar de lista cliente y empleado

--- src/app/modules/administrador-car/list-clientes/pages/list-clientes/list-clientes.component.ts
+++ src/app/modules/administrador-car/list-clientes/pages/list-clientes/list-clientes.component.ts
@@ -3,6 +3,7 @@ import { PersonasService } from 'src/app/shared/services/personas.service';
 import { RolesService } from 'src/app/shared/services/roles.service';
 import { UsuarioService } from 'src/app/shared/services/usuario.service';
 import { Usuario } from 'src/app/core/models/usuario';
+import { Persona } from 'src/app/core/models/persona';
 
 @Component({
   selector: 'app-list-clientes',
@@ -13,6 +14,9 @@ export class ListClientesComponent implements OnInit {
 
   usuariosFiltrados: Usuario[]=[];
   buscar='';
+  usuario: Usuario = new Usuario();
+  persona: Persona = new Persona();
+
   constructor( private personaser: PersonasService, private rolService: RolesService, private userService:UsuarioService){}
   ngOnInit(): void {
     this.userService.getUsuarios().subscribe(usuarios => {
@@ -20,5 +24,74 @@ export class ListClientesComponent implements OnInit {
     });
   }
 
+
+  userSeleccionada: Usuario  | undefined;
+
+  seleccionarUsuario(usua: Usuario,id:number) {
+    this.userSeleccionada = usua;
+
+    this.userService.getPorId(id).subscribe(
+      data =>{
+        console.log(data)
+      }
+    )
+  }
+
+  buscarporid(id: string){
+
+    this.userService.getPorId(id).subscribe(
+      data =>{
+        console.log(data)
+      }
+    )
+
+    }
+
+
+
+  editarusuario(persona:Persona ,usuario:Usuario,id:number){
+
+
+
+    this.userService.getPorId(id).subscribe(
+      data =>{
+        console.log(data)
+        const idper = data.persona.id_persona
+        const per =  data.persona
+
+       this.userService.updateUsuario(usuario,id).subscribe(
+         data=>{
+
+           console.log(data);
+
+
+           this.usuario.nombreUsuario = data.nombreUsuario
+           this.usuario.password = data.password
+           this.usuario = data;
+         },
+         error => {
+          console.error(error);
+        }
+       )
+       this.personaser.updatePersona( persona, idper).subscribe(
+        data=>{
+          console.log(data)
+          this.persona.nombre = data.nombre
+          this.persona.apellido = data.apellido
+          this.persona.ciudad = data.ciudad
+          this.persona.correo = data.correo
+          this.persona.direccion = data.direccion
+          this.persona.genero = data.genero
+          this.persona.telefono = data.telefono
+        }
+       )
+      },
+      error => {
+        console.error(error);
+      }
+
+    )
+
+  }
 
 }
--- src/app/modules/administrador-car/list-empleado/pages/list-empleado/list-empleado.component.ts
+++ src/app/modules/administrador-car/list-empleado/pages/list-empleado/list-empleado.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Persona } from 'src/app/core/models/persona';
 import { Usuario } from 'src/app/core/models/usuario';
 import { PersonasService } from 'src/app/shared/services/personas.service';
 import { UsuarioService } from 'src/app/shared/services/usuario.service';
@@ -13,11 +14,86 @@ export class ListEmpleadoComponent {
 
   usuariosFiltrados: Usuario[]=[];
   buscar='';
-
-  constructor( private persona: PersonasService,private  usu:UsuarioService){}
+  usuario: Usuario = new Usuario();
+  persona: Persona = new Persona();
+  constructor( private personaser: PersonasService,private  userService:UsuarioService){}
   ngOnInit(): void {
-    this.usu.getUsuarios().subscribe(usuarios => {
+    this.userService.getUsuarios().subscribe(usuarios => {
       this.usuariosFiltrados = usuarios.filter(usuario => usuario.rol.id_rol === 2)
     });
   }
+
+  userSeleccionada: Usuario  | undefined;
+
+  seleccionarUsuario(usua: Usuario,id:number) {
+    this.userSeleccionada = usua;
+
+    this.userService.getPorId(id).subscribe(
+      data =>{
+        console.log(data)
+      }
+    )
+  }
+
+  buscarporid(id: string){
+
+    this.userService.getPorId(id).subscribe(
+      data =>{
+        console.log(data)
+      }
+    )
+
+    }
+
+
+  editarusuario(persona:Persona ,usuario:Usuario,id:number){
+
+
+
+    this.userService.getPorId(id).subscribe(
+      data =>{
+        console.log(data)
+        const idper = data.persona.id_persona
+        const per =  data.persona
+
+       this.userService.updateUsuario(usuario,id).subscribe(
+         data=>{
+
+           console.log(data);
+
+
+           this.usuario.nombreUsuario = data.nombreUsuario
+           this.usuario.password = data.password
+           this.usuario = data;
+         },
+         error => {
+          console.error(error);
+        }
+       )
+       this.personaser.updatePersona( persona, idper).subscribe(
+        data=>{
+          console.log(data)
+          this.persona.nombre = data.nombre
+          this.persona.apellido = data.apellido
+          this.persona.ciudad = data.ciudad
+          this.persona.correo = data.correo
+          this.persona.direccion = data.direccion
+          this.persona.genero = data.genero
+          this.persona.telefono = data.telefono
+        }
+       )
+      },
+      error => {
+        console.error(error);
+      }
+
+    )
+
+  }
+
+
+
+
+
+
 }
