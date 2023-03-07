af77610601f8c5305ad0cf2c98a04b0eaa0837d2
Saul Velecela <claudio.velecela.est@tecazuay.edu.ec>
2023-03-06 21:25:46 -0500

    listar clase

--- src/app/modules/administrador-car/list-clase/list-clase.module.ts
+++ src/app/modules/administrador-car/list-clase/list-clase.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ListClaseRoutingModule } from './list-clase-routing.module';
 import { ListClaseComponent } from './pages/list-clase/list-clase.component';
+import { BuscarPipe } from './buscar.pipe';
 
 
 @NgModule({
   declarations: [
-    ListClaseComponent
+    ListClaseComponent,
+    BuscarPipe
   ],
   imports: [
     CommonModule,
