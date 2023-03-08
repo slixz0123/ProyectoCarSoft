c1bdc8e43a13a4d8ef83e87715989db3c27d53f6
slixz0123 <slim.ph2002@gmail.com>
2023-03-07 19:13:07 -0500

    actualizacion  y eliminado de clase

--- src/app/modules/administrador-car/list-clase/pages/list-clase/list-clase.component.ts
+++ src/app/modules/administrador-car/list-clase/pages/list-clase/list-clase.component.ts
@@ -10,10 +10,82 @@ import { ClasesCarroService } from 'src/app/shared/services/clases-carro.service
 export class ListClaseComponent {
 buscar='';
 clases: Claseautomovil[]= [];
+clasea: Claseautomovil = new Claseautomovil();
 constructor(private clase: ClasesCarroService){}
 ngOnInit():void{
   this.clase.getAll().subscribe(
     clas=>this.clases=clas
   )
 }
+
+ClaseSeleccionada: Claseautomovil  | undefined;
+
+seleccionarCarro(cl: Claseautomovil,id_persona:number) {
+  this.ClaseSeleccionada = cl;
+
+  this.clase.getPorId(id_persona).subscribe(
+    data =>{
+      console.log(data)
+    }
+  )
+}
+
+
+
+
+
+
+editarclase(clasea:Claseautomovil,id_clase:number){
+
+
+
+  this.clase.getPorId(id_clase).subscribe(
+    data =>{
+      console.log(data)
+
+     this.clase.update(clasea,id_clase).subscribe(
+       data=>{
+         console.log(data);
+
+         this.clasea.nombre = data.nombre
+         this.clasea.precio_alquiler_dia = data.precio_alquiler_dia
+
+
+         this.clasea = data;
+       },
+       error => {
+        console.error(error);
+      }
+     )
+    },
+    error => {
+      console.error(error);
+    }
+
+  )
+
+}
+
+
+
+
+
+eliminar(id_clase: string){
+  // this.automovilService.getPorId()
+
+   this.clase.delete(id_clase).subscribe(
+     data=>{
+       console.log(data);
+
+
+       this.clasea = data;
+     }
+   )
+
+ }
+
+
+
+
+
 }
--- src/app/shared/services/clases-carro.service.ts
+++ src/app/shared/services/clases-carro.service.ts
@@ -27,11 +27,11 @@ export class ClasesCarroService {
   }
 
   update(claseAutomovil: Claseautomovil, idClase: any) {
-    return this.http.put<Claseautomovil>(this.URL + `updateclase/${idClase}`, claseAutomovil);
+    return this.http.put<Claseautomovil>(this.URL + `actualizar/${idClase}`, claseAutomovil);
   }
 
   delete( idClase: any) {
-    return this.http.delete<boolean>(this.URL + `borrarclase/${idClase}`);
+    return this.http.delete<Claseautomovil>(this.URL + `eliminar/${idClase}`);
   }
 
 
