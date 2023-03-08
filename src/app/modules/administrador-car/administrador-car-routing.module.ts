1e06916f67c8c1836e956922d866e735e5ac07f8
slixz0123 <slim.ph2002@gmail.com>
2023-03-07 20:13:51 -0500

    Update administrador-car-routing.module.ts

--- src/app/modules/administrador-car/administrador-car-routing.module.ts
+++ src/app/modules/administrador-car/administrador-car-routing.module.ts
@@ -8,10 +8,7 @@ const routes: Routes = [
       path: 'register-car',
       loadChildren: () => import("./register-car/register-car.module").then(m => m.RegisterCarModule)
     },
-    {
-      path: 'edit-car',
-      loadChildren: () => import("./edit-car/edit-car.module").then(m => m.EditCarModule)
-    },
+
     {
       path: 'list-car',
       loadChildren: () => import("./list-car/list-car.module").then(m => m.ListCarModule)
@@ -22,10 +19,7 @@ const routes: Routes = [
       path: 'register-employee',
       loadChildren: () => import("./register-empleado/register-empleado.module").then(m => m.RegisterEmpleadoModule)
     },
-    {
-      path: 'edit-employee',
-      loadChildren: () => import("./edit-empleado/edit-empleado.module").then(m => m.EditEmpleadoModule)
-    },
+
     {
       path: 'list-employees',
       loadChildren: () => import("./list-empleado/list-empleado.module").then(m => m.ListEmpleadoModule)
@@ -36,10 +30,7 @@ const routes: Routes = [
       path: 'register-class',
       loadChildren: () => import("./register-clase/register-clase.module").then(m => m.RegisterClaseModule)
     },
-    {
-      path: 'edit-class',
-      loadChildren: () => import("./edit-clase/edit-clase.module").then(m => m.EditClaseModule)
-    },
+
     {
       path: 'list-class',
       loadChildren: () => import("./list-clase/list-clase.module").then(m => m.ListClaseModule)
