465a93c7ee299bde855c2417f8f616aa85c9dd0e
slixz0123 <slim.ph2002@gmail.com>
2023-03-07 19:50:09 -0500

    arreglo

---
+++ src/app/modules/administrador-car/list-seguro.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ListSeguroPipe } from './list-seguro.pipe';
+
+describe('ListSeguroPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ListSeguroPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
--- src/app/modules/administrador-car/list-seguro/list-seguro.module.ts
+++ src/app/modules/administrador-car/list-seguro/list-seguro.module.ts
@@ -5,12 +5,14 @@ import { ListSeguroRoutingModule } from './list-seguro-routing.module';
 import { ListSeguroComponent } from './pages/list-seguro/list-seguro.component';
 import { BuscarPipe } from '../list-clase/buscar.pipe';
 import { FormsModule } from '@angular/forms';
+import { ListSeguroPipe } from '../list-seguro.pipe';
 
 
 @NgModule({
   declarations: [
     ListSeguroComponent,
-    BuscarPipe
+    BuscarPipe,
+
   ],
   imports: [
     CommonModule,
