535f9d2338423bb168e3a392ba061c0cda65689b
Diana Zhizhin <dzhizhin632@gmail.com>
2023-03-10 01:54:00 -0500

    Revert "Merge branch 'camilo' of https://github.com/slixz0123/ProyectoCarSoft into camilo"
    This reverts commit 176f2ec9432ce6f161cdced9d544e201b00ed792, reversing
    changes made to c509aa23dd9057c62fd40b9e7b5fbe115097b8fa.

--- src/app/core/models/alquiler.ts
+++ src/app/core/models/alquiler.ts
@@ -9,13 +9,14 @@ import { Usuario } from "./usuario";
 export class Alquiler{
   id_alquiler?: number;
   documento_garantia?:string;
-  fecha_iniresv?:Date;
-  fecha_finresv?:Date;
+  fecha_salida?:Date;
+  prox_fecha_entrega?:Date;
 
 
 
   //relacion
   usuario: Usuario = new Usuario(); // id de usuario
+  rol: Rol = new Rol(); ///cod_comp codigo comprobante
   seguro: Seguro= new Seguro(); // cod_resv codigo de reserva
   auto: Automovil= new Automovil();
 
